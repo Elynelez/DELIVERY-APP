13c6009426531fc524fe5a723eb2ff221eb2a9fc
Elynelez <Elinarnyex@gmail.com>
2023-11-28 15:21:24 -0500

    user.email en las PackagePages

--- src/pages/PackagePages/EnterProduct.jsx
+++ src/pages/PackagePages/EnterProduct.jsx
@@ -11,7 +11,7 @@ const EnterProduct = () =>{
                 overflow="hidden"
                 allowtransparency="true"
                 frameborder="0"
-                src={"https://script.google.com/macros/s/AKfycbw2qnpT92wXJf9liQBQVTILWo95maaZ7Y6S0VVYvlynt0UOVSw4loZZUebd9_UFX4bq/exec/enter?user="+user}
+                src={"https://script.google.com/macros/s/AKfycbw2qnpT92wXJf9liQBQVTILWo95maaZ7Y6S0VVYvlynt0UOVSw4loZZUebd9_UFX4bq/exec/enter?user="+user.email}
                 className="iframe-content"
             />
         </div>
--- src/pages/PackagePages/ExitProduct.jsx
+++ src/pages/PackagePages/ExitProduct.jsx
@@ -11,7 +11,7 @@ const ExitProduct = () =>{
                 overflow="hidden"
                 allowtransparency="true"
                 frameborder="0"
-                src={"https://script.google.com/macros/s/AKfycbw2qnpT92wXJf9liQBQVTILWo95maaZ7Y6S0VVYvlynt0UOVSw4loZZUebd9_UFX4bq/exec/exit?user="+user}
+                src={"https://script.google.com/macros/s/AKfycbw2qnpT92wXJf9liQBQVTILWo95maaZ7Y6S0VVYvlynt0UOVSw4loZZUebd9_UFX4bq/exec/exit?user="+user.email}
                 className="iframe-content"
             />
         </div>
