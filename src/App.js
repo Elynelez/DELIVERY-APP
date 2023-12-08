95690022910bf4c5521715786abbd39bd28afc0d
Elynelez <Elinarnyex@gmail.com>
2023-12-07 19:02:12 -0500

    usuario andres para salidas y entradas

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ const logisticEmails = ["pedidos.ducor@gmail.com", "logistica.inducor@gmail.com"
 const bossEmails = ["pedidos.ducor@gmail.com", "contableducor@gmail.com", "inducorsas@gmail.com"]
 const sellerEmails = ["pedidos.ducor@gmail.com"]
 const ExternalServiceEmails = ["atencionalcliente.magicmechas@gmail.com"]
-const entriesInventoryEmails = ["pedidos.ducor@gmail.com", "inducorsas@gmail.com","ainducor@gmail.com"]
-const exitsInventoryEmails = ["pedidos.ducor@gmail.com", "inducorsas@gmail.com","empaque.inducor@gmail.com"]
+const entriesInventoryEmails = ["aocampo.inducor@gmail.com","pedidos.ducor@gmail.com", "inducorsas@gmail.com","ainducor@gmail.com"]
+const exitsInventoryEmails = ["aocampo.inducor@gmail.com","pedidos.ducor@gmail.com", "inducorsas@gmail.com","empaque.inducor@gmail.com"]
 
 
 
