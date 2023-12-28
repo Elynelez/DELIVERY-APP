0435b457dad6562c4ebdad1968969e94a42bb7e8
Elynelez <Elinarnyex@gmail.com>
2023-12-28 18:24:57 -0500

    doceava prueba

--- src/App.js
+++ src/App.js
@@ -49,10 +49,9 @@ const entriesInventoryEmails = ["pedidos.ducor@gmail.com", "inducorsas@gmail.com
 const exitsInventoryEmails = ["pedidos.ducor@gmail.com", "inducorsas@gmail.com", "aocampo.inducor@gmail.com", "aforero.inducor@gmail.com", "empaque.inducor@gmail.com", "londono.ducor89@gmail.com", "pbello.inducor@gmail.com"]
 const settingInventoryEmails = ["aocampo.inducor@gmail.com", "rramirez.inducor@gmail.com"]
 
-const socket = io(process.env.NODE_ENV === 'production' ? '/' : `http://localhost:${PORT}`);
+const socket = io('http://localhost:8080', { transports: ['websocket'] });
 
 function App() {
-  console.log(process.env.NODE_ENV)
   const { isAuthenticated, user } = useAuth0();
   const [theme, colorMode] = useMode();
 
