720f4a8f848491ac585f13edd05a840c91da4d51
Elynelez <Elinarnyex@gmail.com>
2024-01-24 11:37:46 -0500

    funciones con servidor 2

--- src/pages/PackagePages/ExitProduct.jsx
+++ src/pages/PackagePages/ExitProduct.jsx
@@ -6,7 +6,8 @@ import { v4 } from 'uuid';
 import { useAuth0 } from '@auth0/auth0-react';
 import { io } from "socket.io-client";
 import { loadData } from "../../middlewares";
-const socket = io('http://localhost:' + 8080);
+// const socket = io('http://localhost:' + 8080);
+const socket = io("https://server-cloud-mggp.onrender.com")
 
 const ExitProduct = ({ rangeItems, setRangeItems, pendingData, setPendingData }) => {
     const { user } = useAuth0();
